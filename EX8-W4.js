f8bd05e73b3e605f2ab7433389160f592461bbce
wildan <wildanagung34@gmail.com>
2019-01-20 21:34:19 +0700

    upload tusag w4

---
+++ EX8-W4.js
@@ -0,0 +1,18 @@
+function tukarBesarKecil(kalimat) {
+  var ubah = ''
+  for (var i = 0; i < kalimat.length; i++){
+    if (kalimat[i] !== kalimat[i].toLowerCase()){
+      ubah += kalimat[i].toLowerCase()
+    }else{
+      ubah += kalimat[i].toUpperCase()
+    }
+  }
+  return ubah
+}
+
+// TEST CASES
+console.log(tukarBesarKecil('Hello World')); // "hELLO wORLD"
+console.log(tukarBesarKecil('I aM aLAY')); // "i Am Alay"
+console.log(tukarBesarKecil('My Name is Bond!!')); // "mY nAME IS bOND!!"
+console.log(tukarBesarKecil('IT sHOULD bE me')); // "it Should Be ME"
+console.log(tukarBesarKecil('001-A-3-5TrdYW')); // "001-a-3-5tRDyw"
